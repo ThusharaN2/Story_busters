302e2f7990dd6a2bff47547709666b906c06d84d
Amera Alleyne <amera.alleyne@gmail.com>
2022-05-10 10:08:31 -0700

    unfinished

--- routes/register.js
+++ routes/register.js
@@ -1,22 +1,31 @@
-router.get("/register", (req, res) => {
+const express = require('express');
+const router  = express.Router();
+
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const { email, password } = req.body
+  db.query(`INSERT INTO users (email, password) VALUES (${email}, ${password}};`)
 
   res.render("register");
   // redirect to /users/profile
 });
 router.post("/register", (req, res) => {
-const { email, password } = req.body
 
 console.log(req.body);
 
-db.query(`INSERT INTO users (email, password) VALUES (${email}, ${password}};`)
-.then(data => {
- // const email = data.columns;
-  res.redirect("users");
-  // redirect to /users/profile
-})
-.catch(err => {
-  res
-    .status(500)
-    .json({ error: err.message });
+
+// .then(data => {
+//  // const email = data.columns;
+//   res.redirect("users");
+//   // redirect to /users/profile
+// })
+// .catch(err => {
+//   res
+//     .status(500)
+//     .json({ error: err.message });
+// });
+// });
 });
-});
+return router;
+};
