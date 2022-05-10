98ab2cf30d5ff211364df20cd838ec32d9142f3c
Amera Alleyne <amera.alleyne@gmail.com>
2022-05-10 09:58:25 -0700

    unfinished -AA

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,15 @@
 // app.post("/home", (req, res) => {
 //   res.render("/home");
 // });
+
 const express = require('express');
 const router  = express.Router();
 
-
-
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT users.email FROM users;`)
-      .then(data => {
-        const email = data.columns;
-        res.json({ email });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
+    const userEmail = req.params.email;
+   const userPassword = req.params.password;
+   console.log(userEmail)
+   console.log(userPassword)
+  })
 };
