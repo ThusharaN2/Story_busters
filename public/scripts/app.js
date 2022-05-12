764e99899da240982cfa42825c6d3f1abaefbf60
Amera Alleyne <amera.alleyne@gmail.com>
2022-05-12 13:33:29 -0700

    upvote button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@
 // Client facing scripts here
 // const loveMarkup = `&#128512`
 $(document).ready(function() {
+console.log("hello doc");
   $("#story_1").click(function() {
 
     $(this).prepend(
@@ -44,5 +45,34 @@ $(document).ready(function() {
      `❤️ `
     );
   });
+
+  const upvoteButtons = [...document.querySelectorAll(".upvote")];
+  console.log(upvoteButtons);
+
+  upvoteButtons.forEach(upvoteButton => {
+    console.log("hello")
+    upvoteButton.addEventListener("click", () => {
+      // getAttribute getting the Id.
+      const upvoteId = upvoteButton.getAttribute("data-upvoteID");
+      console.log(`I clicked on the upvote ${upvoteId}.`);
+
+      $.ajax({
+        url: "/add-to-stories/upvotes",
+        data: { "upvoteID": upvoteId },
+        method: "POST",
+        type: "application/json",
+        success: function (data) {
+          console.log("i got here", data);
+          //this will auto reload the page
+          window.location.reload()
+        },
+        error: function (err) {
+          console.log(err);
+        }
+
+      })
+    })
+
+  })
 
 });
