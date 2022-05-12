bf62986ce30d2f02ef4aff72211c7b26b594af40
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-11 23:48:28 -0400

    add hearts to titles when clicked

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,48 @@
 // Client facing scripts here
+// Client facing scripts here
+// const loveMarkup = `&#128512`
+$(document).ready(function() {
+  $("#story_1").click(function() {
+
+    $(this).prepend(
+     `❤️ `
+    );
+  });
+
+  $("#story_2").click(function() {
+
+    $(this).prepend(
+     `❤️ `
+    );
+  });
+
+  $("#story_3").click(function() {
+
+    $(this).prepend(
+     `❤️ `
+    );
+  });
+
+
+  $("#story_4").click(function() {
+
+    $(this).prepend(
+     `❤️ `
+    );
+  });
+
+  $("#story_5").click(function() {
+
+      $(this).prepend(
+       `❤️ `
+      );
+  });
+
+  $("#story_6").click(function() {
+
+    $(this).prepend(
+     `❤️ `
+    );
+  });
+
+});
