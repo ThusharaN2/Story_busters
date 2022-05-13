a75c6be19837a1ad8fa110351c4db76b749b43b7
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-12 21:45:42 -0400

    new seed data for presenting

--- db/seeds/03_proposed_additions.sql
+++ db/seeds/03_proposed_additions.sql
@@ -1,15 +1,38 @@
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (2, 'Oh no!!! I think it is a skunk, I got to get out of here fast, or that scent will linger for a week!', 1);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'It was a huge dog! Thank God, because that could have ended really bad.', 1);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (1, '...or maybe you can because nothing happened.', 2);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'I am pretty sure that the pizza tastes better here.', 2);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (4, 'I just found my old password for my bitcoin wallet so I am officially rich!', 2);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (5, 'The pizza guy came after the 30 minutes. So I got it for free!', 2);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (1, 'I forgot what I was talking about, so give me a second here. Oh yeah, wait no, that was not it.', 3);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (2, 'The real estate market here is crazy, but he people here are crazier. I love it!', 3);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (4, 'Mama did not raise no fool, no she did not.', 3);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (5, 'You just won''t let me finish talking, will you...', 3);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'I am pretty sure that the rats have outnumbered humans!', 4);
-INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'You can expect to get mugged on a monthly basis.', 4);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (2, 'Oh no!!! I think it is a skunk, I got to get out of here fast, or that scent will linger for a week!', 1);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'It was a huge dog! Thank God, because that could have ended really bad.', 1);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (1, '...or maybe you can because nothing happened.', 2);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'I am pretty sure that the pizza tastes better here.', 2);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (4, 'I just found my old password for my bitcoin wallet so I am officially rich!', 2);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (5, 'The pizza guy came after the 30 minutes. So I got it for free!', 2);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (1, 'I forgot what I was talking about, so give me a second here. Oh yeah, wait no, that was not it.', 3);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (2, 'The real estate market here is crazy, but he people here are crazier. I love it!', 3);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (4, 'Mama did not raise no fool, no she did not.', 3);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (5, 'You just won''t let me finish talking, will you...', 3);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'I am pretty sure that the rats have outnumbered humans!', 4);
+-- INSERT INTO proposed_additions (contributor_id, additional_text, story_id) VALUES (3, 'You can expect to get mugged on a monthly basis.', 4);
+
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (2, 'Oh no!!! I think it is a skunk, I got to get out of here fast, or that scent will linger for a week!', 11, 1);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (3, 'It was a huge dog! Thank God, because that could have ended really bad.', 1, 6);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (1, '...or maybe you can because nothing happened.', 2, 2);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (3, 'I am pretty sure that the pizza tastes better here.', 2, 58);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (4, 'I just found my old password for my bitcoin wallet so I am officially rich!', 2, 19);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (5, 'The pizza guy came after the 30 minutes. So I got it for free!', 2, 3);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (1, 'I forgot what I was talking about, so give me a second here. Oh yeah, wait no, that was not it.', 3, 11);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (2, 'The real estate market here is crazy, but the people here are crazier. I love it!', 3, 7);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (4, 'Mama did not raise no fool, no she did not.', 11, 23);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (5, 'You just won''t let me finish talking, will you...', 3, 2);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (3, 'I am pretty sure that the rats have outnumbered humans!', 4, 10);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (3, 'You can expect to get mugged on a monthly basis.', 4, 0);
+
+
+
+-- For frog story (bitter tale)
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (4, 'and then he forgot how to rhyme.', 14, 3);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (3, 'then his eyes rolled into the pond.', 14, 4);
+INSERT INTO proposed_additions (contributor_id, additional_text, story_id, likes) VALUES (4, 'then he discovered pizza in the fridge', 14, 100);
+
+
+
 
 
 
